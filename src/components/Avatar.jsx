38176612c27dd9ff362ad5f6fb74a3b574b9d1d1
cc-robinson <candace.robinson@c-mci.com>
2024-09-11 15:57:21 -0400

    update avatar

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -188,7 +188,11 @@ export function Avatar(props) {
         material={materials.Wolf3D_Hair}
         skeleton={nodes.Wolf3D_Hair.skeleton}
       />
-      
+      <skinnedMesh
+        geometry={nodes.Wolf3D_Glasses.geometry}
+        material={materials.Wolf3D_Glasses}
+        skeleton={nodes.Wolf3D_Glasses.skeleton}
+      />
       <skinnedMesh
         geometry={nodes.Wolf3D_Body.geometry}
         material={materials.Wolf3D_Body}
